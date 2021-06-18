394436f2b241186928cd67268bcd79465bf2e85d
tomotsuka.masaki <tomotsuka.masaki@pocke.co.jp>
2021-06-18 19:45:12 +0900

    fix: tab to spaces

--- src/consts.ts
+++ src/consts.ts
@@ -1,18 +1,18 @@
 export const TITLE = "PANEL FLIPS"
 
 export const BOARD_SIZES = Object.freeze([
-	{
-		name: 'かんたん',
-		value: 3
-	},
-	{ 
-		name: 'むずかしい',
-		value: 5
-	},
-	{
-		name: 'オニ',
-		value: 8
-	}
+  {
+    name: 'かんたん',
+    value: 3
+  },
+  {
+    name: 'むずかしい',
+    value: 5
+  },
+  {
+    name: 'オニ',
+    value: 8
+  }
 ])
 
 export const ICON_CELLS = 9;
